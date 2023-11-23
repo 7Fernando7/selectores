1a4b2551b5e610a92ec0c2eeefdc08fddde3a161
Pereira De Paiva <fernando.pereira@transfesa.com>
2023-11-23 09:09:42 +0100

    segundo selector anidado -Países, Obtener países por region.

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,8 +1,9 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region } from '../../interfaces/country.interfaces';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'selector-page',
@@ -12,7 +13,7 @@ import { Region } from '../../interfaces/country.interfaces';
   ]
 })
 
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit {
 
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required ],
@@ -26,8 +27,24 @@ export class SelectorPageComponent {
     private countriesService: CountriesService,
   ) {}
 
+
+  ngOnInit(): void {
+    this.onRegionChanged();
+  }
+
   get regions(): Region[] {
     return this.countriesService.regions;
+  }
+
+  onRegionChanged(): void {
+
+    this.myForm.get('region')!.valueChanges
+    .pipe(
+      switchMap( region => this.countriesService.getCountriesByRegion(region) ),
+    )
+      .subscribe( region => {
+        console.log({ region })
+      });
   }
 
 
